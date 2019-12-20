e9c85b856789eaae146668fed36d92354cef8230
Paulo Lemos Neto <plneto@gmail.com>
2019-12-20 22:36:52 +0000

    refactored code

--- src/TorrentExtractor.ConsoleApp/Program.cs
+++ src/TorrentExtractor.ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using CommandLine;
+using MediatR;
 using Serilog;
 using TorrentExtractor.Core.Helpers;
 using TorrentExtractor.Core.Infrastructure;
@@ -35,34 +36,7 @@ namespace TorrentExtractor.ConsoleApp
 
             try
             {
-                args = ArgsHelper.RemoveEmptyArgs(args);
-
-                if (args.Length > 0)
-                    Log.Debug("Args: {0}", string.Join(" ", args));
-                else
-                    Log.Information("No args provided");
-
-                var options = Parser.Default.ParseArguments<TorrentOptions>(args);
-
-                if (options.Tag == ParserResultType.NotParsed)
-                {
-                    Log.Error("Error parsing arguments");
-
-                    var errors = new List<Error>();
-                    options.WithNotParsed(x => errors = x.ToList());
-
-                    foreach (var error in errors)
-                    {
-                        Log.Error(error.ToString());
-                    }
-
-                    Log.Information("Program Exited");
-                    return;
-                }
-
-                var torrentOptions = new TorrentOptions();
-
-                options.WithParsed(x => torrentOptions = x);
+                var torrentOptions = GetTorrentOptions(args);
 
                 var isTvShow = torrentOptions.Category == torrentSettings.TvShowsCategory
                                || (string.IsNullOrWhiteSpace(torrentOptions.Category)
@@ -126,24 +100,6 @@ namespace TorrentExtractor.ConsoleApp
 
             Configuration = configBuilder.Build();
 
-            IServiceCollection services = new ServiceCollection();
-
-            services.AddTransient<IFileFinder, FileFinder>();
-            services.AddTransient<IFileFormatter, FileFormatter>();
-            services.AddTransient<IFileHandler, FileHandler>();
-
-            var torrentSettings = new TorrentSettings();
-            var emailSettings = new EmailSettings();
-            var loggingSettings = new LoggingSettings();
-
-            Configuration.GetSection(nameof(TorrentSettings)).Bind(torrentSettings);
-            Configuration.GetSection(nameof(EmailSettings)).Bind(emailSettings);
-            Configuration.GetSection(nameof(LoggingSettings)).Bind(loggingSettings);
-
-            services.AddSingleton(ctx => torrentSettings);
-            services.AddSingleton(ctx => emailSettings);
-            services.AddSingleton(ctx => loggingSettings);
-
             var logFilePath = Configuration["LoggingSettings:LogFilePath"];
 
             Log.Logger = new LoggerConfiguration()
@@ -154,9 +110,61 @@ namespace TorrentExtractor.ConsoleApp
 
             Log.Information("Logger online");
 
+            var torrentSettings = new TorrentSettings();
+            var emailSettings = new EmailSettings();
+            var loggingSettings = new LoggingSettings();
+
+            Configuration.GetSection(nameof(TorrentSettings)).Bind(torrentSettings);
+            Configuration.GetSection(nameof(EmailSettings)).Bind(emailSettings);
+            Configuration.GetSection(nameof(LoggingSettings)).Bind(loggingSettings);
+
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton(ctx => torrentSettings);
+            services.AddSingleton(ctx => emailSettings);
+            services.AddSingleton(ctx => loggingSettings);
+
+            services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddTransient<IFileFinder, FileFinder>();
+            services.AddTransient<IFileFormatter, FileFormatter>();
+            services.AddTransient<IFileHandler, FileHandler>();
             services.AddTransient<INotificationService, NotificationService>();
 
             ServiceProvider = services.BuildServiceProvider();
+        }
+
+        public static TorrentOptions GetTorrentOptions(string[] args)
+        {
+            args = ArgsHelper.RemoveEmptyArgs(args);
+
+            if (args.Length > 0)
+                Log.Debug("Args: {0}", string.Join(" ", args));
+            else
+                Log.Information("No args provided");
+
+            var options = Parser.Default.ParseArguments<TorrentOptions>(args);
+
+            if (options.Tag == ParserResultType.NotParsed)
+            {
+                Log.Error("Error parsing arguments");
+
+                var errors = new List<Error>();
+                options.WithNotParsed(x => errors = x.ToList());
+
+                foreach (var error in errors)
+                {
+                    Log.Error(error.ToString());
+                }
+
+                throw new Exception("Failed to parse arguments");
+            }
+
+            var torrentOptions = new TorrentOptions();
+
+            options.WithParsed(x => torrentOptions = x);
+
+            return torrentOptions;
         }
 
         private static bool IsFile(string path)
